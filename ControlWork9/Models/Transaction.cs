ec9ddee595356a245d466056512fdc28631c4741
Tukhfatullin Zakir <zakirtuckhfatullin@gmail.com>
2024-12-28 18:51:37 +0600

    Transaction model added

---
+++ ControlWork9/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlWork9.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    [Required]
+    [Remote(action: "CheckAccountNumber", controller: "Validation", ErrorMessage = "Нет такого номера счёта!")]
+    public int AccountNumber { get; set; }
+    public DateTime? Date { get; set; }
+    [Remote(action: "CheckAmount", controller: "Validation", ErrorMessage = "Нельзя пополнить счёт на такую сумму!")]
+    public decimal Amount { get; set; }
+    
+    public int? SenderId { get; set; }
+    public User? Sender { get; set; }
+    
+    public int? RecieverId { get; set; }
+    public User? Reciever { get; set; }
+}
