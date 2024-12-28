e04d6da7a14b17c5877058b333d954e07c5d3661
Tukhfatullin Zakir <zakirtuckhfatullin@gmail.com>
2024-12-28 19:08:01 +0600

    Program file edited

--- ControlWork9/Program.cs
+++ ControlWork9/Program.cs
@@ -1,27 +1,63 @@
-var builder = WebApplication.CreateBuilder(args);
+using ControlWork9.Models;
+using ControlWork9.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+        string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<WalletContext>(options => options.UseNpgsql(connection))
+            .AddIdentity<User, IdentityRole<int>>(options =>
+            {
+                options.Password.RequiredLength = 5; // минимальная длина
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false; // требуются ли символы в нижнем регистре
+                options.Password.RequireUppercase = false; // требуются ли символы в верхнем регистре
+                options.Password.RequireDigit = false; // требуются ли цифры
+            })
+            .AddEntityFrameworkStores<WalletContext>();
+        var app = builder.Build();
+        using var scope = app.Services.CreateScope();
 
-var app = builder.Build();
+        var services = scope.ServiceProvider;
+        try
+        {
+            var userManager = services.GetRequiredService<UserManager<User>>();
+            var rolesManager = services.GetRequiredService<RoleManager<IdentityRole<int>>>();
+            await AdminInitializer.SeedAdminUser(rolesManager, userManager);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred while seeding the database.");
+        }
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
