93beca8dc6e182f0bc45fcadb6573eb7406bb1cf
Tukhfatullin Zakir <zakirtuckhfatullin@gmail.com>
2024-12-28 18:52:14 +0600

    ServiceUser and SomeProvider models added

---
+++ ControlWork9/Models/ServiceUser.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlWork9.Models;
+
+public class ServiceUser
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public int AccountNumber { get; set; }
+    [Required]
+    public decimal Balance { get; set; }
+        
+    public int ServiceProviderId { get; set; }
+    public SomeProvider SomeProvider { get; set; }
+
+    public int OwnerId { get; set; }
+    public User Owner { get; set; }
+}
---
+++ ControlWork9/Models/SomeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlWork9.Models;
+
+public class SomeProvider
+{
+    public int Id { get; set; }
+        
+    [Required]
+    public string Name { get; set; }
+        
+    public List<ServiceUser>? ServiceUsers { get; set; }
+}
